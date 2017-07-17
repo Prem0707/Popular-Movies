29e9ee41feec98a37dec9b5e396728fff5b983f4
Prem <premprakash0796@gmail.com>
2017-07-18 00:22:52 +0530

    Implementation of AsyncTask for Networking call on background thread

--- app/src/main/java/com/prem/android/popularmovies/Activity/MainActivity.java
+++ app/src/main/java/com/prem/android/popularmovies/Activity/MainActivity.java
@@ -1,14 +1,23 @@
 package com.prem.android.popularmovies.Activity;
 
+import android.graphics.Movie;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.prem.android.popularmovies.Activity.utils.NetworkUtils;
 import com.prem.android.popularmovies.R;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String userSelectedCategory = "popular";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+
+    private class FetchMovieTask extends AsyncTask<String, Void, ArrayList<Movie>> {
+
+        @Override
+        protected ArrayList<Movie> doInBackground(String... params) {
+
+            String sortOptionSelected = params[0];
+            URL urlForFetchMovieDetails = NetworkUtils.buildURL(sortOptionSelected);
+
+            ArrayList<Movie> responseFromAPI = null;
+            if (urlForFetchMovieDetails != null) {
+                try {
+                    responseFromAPI = NetworkUtils.getResponseFromHttpUrl(urlForFetchMovieDetails);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return responseFromAPI;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> movies) {
+            super.onPostExecute(movies);
+        }
     }
 
     @Override
